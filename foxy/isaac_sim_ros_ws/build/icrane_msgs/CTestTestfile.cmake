bae242c75c74381f1ee61488b14e17e68754bfa2
MuriloCastanheira <95063558+MuriloCastanheira@users.noreply.github.com>
2024-06-20 21:32:24 -0300

    Delete foxy/isaac_sim_ros_ws/build directory

--- foxy/isaac_sim_ros_ws/build/icrane_msgs/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /workspace/build_ws/src/icrane_msgs
-# Build directory: /workspace/build_ws/build/icrane_msgs
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("icrane_msgs__py")
