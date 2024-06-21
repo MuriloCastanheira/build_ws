2f77efc14433e74549de4e1f4422007fb7406258
MuriloCastanheira <95063558+MuriloCastanheira@users.noreply.github.com>
2024-06-20 21:32:09 -0300

    Delete foxy/isaac_sim_ros_ws/install directory

--- foxy/isaac_sim_ros_ws/install/share/icrane_msgs/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(icrane_msgs_IDL_FILES "msg/Vector2D.idl;msg/Pose2D.idl;msg/BoundingBox2D.idl;msg/Detection2D.idl;msg/Detection2DArray.idl;msg/DetectionSeg2D.idl;msg/DetectionSeg2DArray.idl;msg/Polygon2D.idl;msg/DetectionSeg3D.idl;msg/DetectionSeg3DArray.idl;msg/OmniverseInfo.idl;msg/TrackedEntities.idl;msg/Modeling.idl")
-set(icrane_msgs_INTERFACE_FILES "msg/Vector2D.msg;msg/Pose2D.msg;msg/BoundingBox2D.msg;msg/Detection2D.msg;msg/Detection2DArray.msg;msg/DetectionSeg2D.msg;msg/DetectionSeg2DArray.msg;msg/Polygon2D.msg;msg/DetectionSeg3D.msg;msg/DetectionSeg3DArray.msg;msg/OmniverseInfo.msg;msg/TrackedEntities.msg;msg/Modeling.msg")
